84fff569deb953cf2110d24ec2adb96bc34a5554
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2024-08-04 10:26:35 +0700

    feat: added sidebar no animation

--- src/app/(sidebar)/layout.tsx
+++ src/app/(sidebar)/layout.tsx
@@ -1,7 +1,14 @@
+import Sidebar from "../components/Sidebar";
+
 export default function SidebarLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex gap-20 px-[5%] py-12">
+      <Sidebar />
+      {children}
+    </div>
+  );
 }
