b41aa42100c8be9d2ecff8764e49c7c185b8df38
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-08-04 23:51:26 +0700

    chore: setup database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
-import { type Config } from "drizzle-kit";
+import { type Config } from 'drizzle-kit';
 
-import { env } from "~/env";
+import { env } from '~/env';
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
+  schema: './node_modules/@katitb2024/database/dist/schema.js',
+  dialect: 'postgresql',
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ["t3-template-app_*"],
+  out: './node_modules/@katitb2024/database/drizzle',
+  tablesFilter: ['*'],
 } satisfies Config;
