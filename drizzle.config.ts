4165e85d4e20884299dd751717dfc9ce2186173d
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-08-05 09:48:56 +0700

    chore: update drizzle package version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import { type Config } from 'drizzle-kit';
 
-import { env } from '~/env';
-
 export default {
   schema: './node_modules/@katitb2024/database/dist/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: 'postgresql://postgres:posgtres@localhost:5432/coba',
   },
   out: './node_modules/@katitb2024/database/drizzle',
   tablesFilter: ['*'],
