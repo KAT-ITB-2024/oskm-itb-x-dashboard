3b6a330bdcacf78fd839243cb8183ac7fcd31eea
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-08-05 11:26:30 +0700

    feat: be login

--- src/app/(login)/component/LoginForm.tsx
+++ src/app/(login)/component/LoginForm.tsx
@@ -1,7 +1,37 @@
+"use client";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <p>LoginForm</p>
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-2 bg-black">
+      <input
+        value={email}
+        onChange={(v) => setEmail(v.target.value)}
+        type="text"
+      />
+      <input
+        value={password}
+        onChange={(v) => setPassword(v.target.value)}
+        type="password"
+      />
+      <button
+        className="bg-white p-2"
+        onClick={async () => {
+          const status = await signIn("credentials", {
+            email,
+            password,
+            redirect: false,
+          });
+
+          console.log(status);
+        }}
+      >
+        Sign In
+      </button>
     </div>
   );
 }
