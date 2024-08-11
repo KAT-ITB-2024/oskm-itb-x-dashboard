a4c5399d5f41dbafd2339a2215c25e0124f64eb8
jon <zhenagajo234@gmail.com>
2024-08-11 21:11:36 +0700

    feat : create the api

--- src/server/api/routers/assignment.ts
+++ src/server/api/routers/assignment.ts
@@ -1,6 +1,8 @@
 // Router ini digunakan untuk segala yang berkaitan dengan assignment (tugas-tugas dan submisi)
+import { assignments, assignmentTypeEnum } from "@katitb2024/database";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import {eq}  from "drizzle-orm";
 
 import {
   createTRPCRouter,
@@ -10,4 +12,25 @@ import {
   //   mametMentorProcedure,
 } from "~/server/api/trpc";
 
-export const assignmentRouter = createTRPCRouter({});
+export const assignmentRouter = createTRPCRouter({
+  getAllMainAssignmentMentor:publicProcedure
+    .query(async({ctx})=>{
+        try{  
+          const res = await ctx.db
+                .select({
+                  judulTugas:assignments.title,
+                  waktuMulai:assignments.startTime,
+                  waktuSelesai:assignments.deadline
+                })
+                .from(assignments)
+                .where(eq(assignments.assignmentType,'Main'))
+
+          return res
+        }catch(error){
+          throw new TRPCError({
+            code:"INTERNAL_SERVER_ERROR",
+            message:"An error occured while getting all main assignment "
+          })
+        }
+    })
+});
