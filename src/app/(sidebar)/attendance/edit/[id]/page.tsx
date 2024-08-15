d25f3dd3eaf63d8fc0ae9511863fb2adfa2448e2
WwzFwz <dzakyaureliafawwaz@gmail.com>
2024-08-15 11:21:33 +0700

    feat : added form edit

--- src/app/(sidebar)/attendance/edit/[id]/page.tsx
+++ src/app/(sidebar)/attendance/edit/[id]/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+
+import { IoChevronBackSharp } from "react-icons/io5";
+
+import DashboardHeader from "~/app/components/DashboardHeader";
+import MametFormAttendance from "../../components/MametFormAttendance";
+
+
 export default function Page() {
   return (
-    <div>
-      <p>Page</p>
-    </div>
+    <>
+      <DashboardHeader title="Edit Event" />
+      <Link href="/attendance" className="my-6 flex items-center gap-3">
+        <IoChevronBackSharp className="cursor-pointer text-2xl text-[#0010A4]" />
+        <h1 className="w-fit bg-gradient-to-r from-[#0010A4] to-[#EE1192] bg-clip-text text-3xl font-bold text-transparent">
+          Edit Tugas
+        </h1>
+      </Link>
+      <MametFormAttendance/>
+    </>
   );
 }
