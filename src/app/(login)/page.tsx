55bb102ca70be70e7f1104fe98a1fd7605c4cac2
jon <zhenagajo234@gmail.com>
2024-08-17 22:58:22 +0700

    fix : delete frontend

--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -1,14 +1,10 @@
-  import { getServerAuthSession } from "~/server/auth";
-import { NewAssignmentFormClient } from "./NewAssignmentFormClient";
-
+import { getServerAuthSession } from "~/server/auth";
+import LoginForm from "./component/LoginForm";
 
 export default async function Page() {
   const session = await getServerAuthSession();
   const user = session?.user;
   console.log(user);
 
-  return (<div>
-      <NewAssignmentFormClient/>
-    </div>
-  )
+  return <LoginForm />;
 }
