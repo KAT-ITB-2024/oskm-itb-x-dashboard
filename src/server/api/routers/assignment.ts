328943ea431fafe6cbc9980c3f94eb51b9d5e73f
jon <zhenagajo234@gmail.com>
2024-08-17 09:28:17 +0700

    fix : change the comparison using assignmentType

--- src/server/api/routers/assignment.ts
+++ src/server/api/routers/assignment.ts
@@ -3,6 +3,7 @@ import { assignments, assignmentTypeEnum } from "@katitb2024/database";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import {eq}  from "drizzle-orm";
+import { AssignmentType } from "@katitb2024/database";
 
 import {
   createTRPCRouter,
@@ -16,6 +17,8 @@ export const assignmentRouter = createTRPCRouter({
   getAllMainAssignmentMentor:publicProcedure
     .query(async({ctx})=>{
         try{  
+          
+          const compare:AssignmentType = 'Main';
           const res = await ctx.db
                 .select({
                   judulTugas:assignments.title,
@@ -23,7 +26,7 @@ export const assignmentRouter = createTRPCRouter({
                   waktuSelesai:assignments.deadline
                 })
                 .from(assignments)
-                .where(eq(assignments.assignmentType,'Main'))
+                .where(eq(assignments.assignmentType,compare ))
 
           return res
         }catch(error){
