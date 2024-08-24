e638d3e309503942f57efa7a11c97cf6eed99bbb
ganadipa <nyomanganadipa@gmail.com>
2024-08-25 03:06:45 +0700

    Feat: Presence

--- src/server/api/routers/assignment.ts
+++ src/server/api/routers/assignment.ts
@@ -1,10 +1,17 @@
 // Router ini digunakan untuk segala yang berkaitan dengan assignment (tugas-tugas dan submisi)
 
-import { assignments, AssignmentType,assignmentTypeEnum, assignmentSubmissions, groups, profiles, users } from "@katitb2024/database";
+import {
+  assignments,
+  AssignmentType,
+  assignmentTypeEnum,
+  assignmentSubmissions,
+  groups,
+  profiles,
+  users,
+} from "@katitb2024/database";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import {eq, and} from "drizzle-orm";
-
+import { eq, and } from "drizzle-orm";
 
 import {
   createTRPCRouter,
@@ -16,101 +23,115 @@ import {
 import { profile } from "console";
 
 type MenteeAssignment = {
-    nama:string
-    nim:string
-    keterlambatan:number|null;
-    nilai: number | null;
-    linkFile:string|null;
-    assignmentSubmissions:string|null;
-
-}
+  nama: string;
+  nim: string;
+  keterlambatan: number | null;
+  nilai: number | null;
+  linkFile: string | null;
+  assignmentSubmissions: string | null;
+};
 
 export const assignmentRouter = createTRPCRouter({
+  getMenteeAssignmentSubmission: publicProcedure
+    .input(
+      z.object({
+        assignmentId: z.string(),
+        groupName: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        const { assignmentId, groupName } = input;
 
-  getMenteeAssignmentSubmission:publicProcedure
-    .input(z.object({
-        assignmentId:z.string(),
-        groupName:z.string()
-    }))
-    .query(async({ctx,input})=>{
-        try{
-            const {assignmentId, groupName} = input;
-
-            const res = await ctx.db
-            .select({
-              nama: profiles.name,
-              nim: users.nim,
-              nilai: assignmentSubmissions.point,
-              linkFile: assignmentSubmissions.file,
-              updatedAt: assignmentSubmissions.updatedAt,
-              deadline: assignments.deadline,
-              assignmentsId:assignmentSubmissions.id,
-            })
-            .from(assignmentSubmissions)
-            .innerJoin(users, eq(assignmentSubmissions.userNim, users.nim))
-            .innerJoin(profiles, eq(users.id, profiles.userId))
-            .innerJoin(assignments, eq(assignmentSubmissions.assignmentId, assignments.id))
-            .where(
-              and(
-                eq(assignmentSubmissions.assignmentId, assignmentId),
-                eq(users.nim, assignmentSubmissions.userNim),
-                eq(profiles.userId, users.id),
-                eq(profiles.group, groupName)
-              )
-            );
-            
-            const resultsWithKeterlambatan = res.map(item => ({
-              ...item,
-              terlambat: item.updatedAt > item.deadline  ?  Math.floor((item.updatedAt.getTime() - item.deadline.getTime()) / 1000) : 0
-            }));
-
-            const allMentee = await ctx.db.select({
-                                                              nama:profiles.name,
-                                                              nim:users.nim
-                                                          })
-                                                        .from(profiles)
-                                                        .where(eq(profiles.group,groupName))
-                                                        .innerJoin(users,eq(users.id,profiles.userId))
-
-
-            const menteeAssignment = allMentee  as MenteeAssignment[];
-
-            menteeAssignment.forEach(mentee=>{
-              const find = resultsWithKeterlambatan.find(r => r.nama === mentee.nama && r.nim === r.nim)
-
-              mentee.keterlambatan = find ? find.terlambat : null;
-              mentee.nilai = find ? find.nilai : null;
-              mentee.linkFile = find ? find.linkFile : null
-              mentee.assignmentSubmissions = find ? find.assignmentsId : null;
-            })
-
-            return menteeAssignment;
-        }catch(error){
-          console.log(error)
-          throw new TRPCError({
-            code:"INTERNAL_SERVER_ERROR",
-            message:"error when fetched all mentee assignment on assignment : ${}"
+        const res = await ctx.db
+          .select({
+            nama: profiles.name,
+            nim: users.nim,
+            nilai: assignmentSubmissions.point,
+            linkFile: assignmentSubmissions.file,
+            updatedAt: assignmentSubmissions.updatedAt,
+            deadline: assignments.deadline,
+            assignmentsId: assignmentSubmissions.id,
           })
-        }
-    }),
-  editMenteeAssignmentPoint:publicProcedure
-    .input(z.object({
-        assignmentId:z.string(),
-        point:z.number(),
-    }))
-    .mutation(async({ctx,input})=>{
-      try{
-            const {assignmentId, point} = input;
+          .from(assignmentSubmissions)
+          .innerJoin(users, eq(assignmentSubmissions.userNim, users.nim))
+          .innerJoin(profiles, eq(users.id, profiles.userId))
+          .innerJoin(
+            assignments,
+            eq(assignmentSubmissions.assignmentId, assignments.id),
+          )
+          .where(
+            and(
+              eq(assignmentSubmissions.assignmentId, assignmentId),
+              eq(users.nim, assignmentSubmissions.userNim),
+              eq(profiles.userId, users.id),
+              eq(profiles.group, groupName),
+            ),
+          );
 
-          let [group] = await ctx.db.select({
-                                              groupName:groups.name,
-                                              point:groups.point
-                                            })
-                                            .from(assignmentSubmissions)
-                                            .where(eq(assignmentSubmissions.id,assignmentId))
-                                            .innerJoin(users,eq(users.nim,assignmentSubmissions.userNim))
-                                            .innerJoin(profiles,eq(profiles.userId, users.id))
-                                            .innerJoin(groups,eq(groups.name, profiles.group))
+        const resultsWithKeterlambatan = res.map((item) => ({
+          ...item,
+          terlambat:
+            item.updatedAt > item.deadline
+              ? Math.floor(
+                  (item.updatedAt.getTime() - item.deadline.getTime()) / 1000,
+                )
+              : 0,
+        }));
+
+        const allMentee = await ctx.db
+          .select({
+            nama: profiles.name,
+            nim: users.nim,
+          })
+          .from(profiles)
+          .where(eq(profiles.group, groupName))
+          .innerJoin(users, eq(users.id, profiles.userId));
+
+        const menteeAssignment = allMentee as MenteeAssignment[];
+
+        menteeAssignment.forEach((mentee) => {
+          const find = resultsWithKeterlambatan.find(
+            (r) => r.nama === mentee.nama && r.nim === r.nim,
+          );
+
+          mentee.keterlambatan = find ? find.terlambat : null;
+          mentee.nilai = find ? find.nilai : null;
+          mentee.linkFile = find ? find.linkFile : null;
+          mentee.assignmentSubmissions = find ? find.assignmentsId : null;
+        });
+
+        return menteeAssignment;
+      } catch (error) {
+        console.log(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message:
+            "error when fetched all mentee assignment on assignment : ${}",
+        });
+      }
+    }),
+  editMenteeAssignmentPoint: publicProcedure
+    .input(
+      z.object({
+        assignmentId: z.string(),
+        point: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const { assignmentId, point } = input;
+
+        let [group] = await ctx.db
+          .select({
+            groupName: groups.name,
+            point: groups.point,
+          })
+          .from(assignmentSubmissions)
+          .where(eq(assignmentSubmissions.id, assignmentId))
+          .innerJoin(users, eq(users.nim, assignmentSubmissions.userNim))
+          .innerJoin(profiles, eq(profiles.userId, users.id))
+          .innerJoin(groups, eq(groups.name, profiles.group));
 
         if (!group) {
           throw new TRPCError({
@@ -119,70 +140,71 @@ export const assignmentRouter = createTRPCRouter({
           });
         }
 
-          let [prevData] = await ctx.db.select({
-                                                          point:assignmentSubmissions.point
-                                                        })
-                                                        .from(assignmentSubmissions)
-                                                        .where(eq(assignmentSubmissions.id,assignmentId))
-          
-          if(group?.point !== undefined && prevData?.point !== null){
-              group.point -= prevData!.point
-          }
-
-        if(!prevData){
-          throw new TRPCError({
-            code:"NOT_FOUND",
-            message:"Assignment submission not found",
+        let [prevData] = await ctx.db
+          .select({
+            point: assignmentSubmissions.point,
           })
+          .from(assignmentSubmissions)
+          .where(eq(assignmentSubmissions.id, assignmentId));
+
+        if (group?.point !== undefined && prevData?.point !== null) {
+          group.point -= prevData!.point;
         }
 
-          prevData!.point = point;
-          group!.point = group!.point + point;
-        
-          await ctx.db.update(assignmentSubmissions)
-                        .set({point:prevData!.point})
-                        .where(eq(assignmentSubmissions.id,assignmentId));
-
-          // update group data
-          const res = await ctx.db.update(groups)
-                .set({point:group!.point})
-                .where(eq(groups.name,group.groupName));
-
-            return {
-                success: true,
-                message: "Mentee assignment point updated successfully",
-                updatedGroup: group
-              };
-        }catch(error){
-          console.log(error)
+        if (!prevData) {
           throw new TRPCError({
-            code:"INTERNAL_SERVER_ERROR",
-            message:`Error when updating mentee assignment point : ${error}`
+            code: "NOT_FOUND",
+            message: "Assignment submission not found",
           });
         }
-       }),
-    getAllMainAssignmentMentor:publicProcedure
-    .query(async({ctx})=>{
-        try{  
-          
-          const compare:AssignmentType = 'Main';
-          const res = await ctx.db
-                .select({
-                  judulTugas:assignments.title,
-                  waktuMulai:assignments.startTime,
-                  waktuSelesai:assignments.deadline
-                })
-                .from(assignments)
-                .where(eq(assignments.assignmentType,compare ))
 
-          return res
-        }catch(error){
-          throw new TRPCError({
-            code:"INTERNAL_SERVER_ERROR",
-            message:"An error occured while getting all main assignment "
-          })
-        }
+        prevData!.point = point;
+        group!.point = group!.point + point;
+
+        await ctx.db
+          .update(assignmentSubmissions)
+          .set({ point: prevData!.point })
+          .where(eq(assignmentSubmissions.id, assignmentId));
+
+        // update group data
+        const res = await ctx.db
+          .update(groups)
+          .set({ point: group!.point })
+          .where(eq(groups.name, group.groupName));
+
+        return {
+          success: true,
+          message: "Mentee assignment point updated successfully",
+          updatedGroup: group,
+        };
+      } catch (error) {
+        console.log(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Error when updating mentee assignment point : ${error}`,
+        });
+      }
     }),
+  getAllMainAssignmentMentor: publicProcedure.query(async ({ ctx }) => {
+    try {
+      const compare: AssignmentType = "Main";
+      const res = await ctx.db
+        .select({
+          judulTugas: assignments.title,
+          waktuMulai: assignments.startTime,
+          waktuSelesai: assignments.deadline,
+        })
+        .from(assignments)
+        .where(eq(assignments.assignmentType, compare));
+
+      return res;
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "An error occured while getting all main assignment ",
+      });
+    }
+  }),
   getMainQuestAssignmentCsv: publicProcedure
     .input(
       z.object({
--- src/server/api/routers/presence.ts
+++ src/server/api/routers/presence.ts
@@ -19,128 +19,417 @@ import {
 } from "~/server/api/trpc";
 import { db } from "~/server/db";
 
-type TAttendanceOfAnEvent = {
+export type TAttendanceOfAnEvent = {
   nim: string;
   name: string;
   group: string;
   presence: "Hadir" | "Izin/Sakit" | "Alpha";
 }[];
 
-type TAttendance = {
+export type TAttendance = {
   day: "Day 1" | "Day 2" | "Day 3" | "Day 4";
   openingOrClosing: "Opening" | "Closing";
   attendance: TAttendanceOfAnEvent;
 };
 
+export type TPresenceBaseResponse<T> = {
+  ok: boolean;
+  message?: string;
+  data: T | null;
+};
+
 export const presenceRouter = createTRPCRouter({
   // Mentor (and perhaps Mamet?)
-  getPresenceOfAnEvent: publicProcedure
+  /**
+   * Get the presence of an event
+   *
+   * -- query --
+   * @param eventId The ID of the event
+   * @param groupName The name of the group
+   * @param page The page number (1-indexed)
+   * @param dataPerPage The number of data per page
+   * @returns TAttendanceOfAnEvent
+   */
+  getPresenceOfAGroupInAnEvent: publicProcedure
     .input(
       z.object({
         eventId: z.string(),
         groupName: z.string().optional(),
+        page: z.number(),
+        dataPerPage: z.number(),
       }),
     )
-    .query<TAttendanceOfAnEvent>(async ({ input: { eventId, groupName } }) => {
-      const returned = await db
+    .query<TPresenceBaseResponse<TAttendanceOfAnEvent>>(
+      async ({ input: { eventId, groupName, page, dataPerPage } }) => {
+        try {
+          const returned = await db
+            .select({
+              nim: users.nim,
+              name: profiles.name,
+              group: profiles.group,
+              presence: eventPresences.presenceType,
+            })
+            .from(events)
+            .innerJoin(eventPresences, eq(events.id, eventPresences.eventId))
+            .innerJoin(users, eq(users.nim, eventPresences.userNim))
+            .innerJoin(profiles, eq(profiles.userId, users.id))
+            .where(
+              and(
+                eq(events.id, eventId),
+                groupName ? eq(profiles.group, groupName) : undefined,
+              ),
+            );
+
+          const paginatedData = returned.slice(
+            dataPerPage * (page - 1),
+            dataPerPage * page,
+          );
+
+          return {
+            ok: true,
+            data: paginatedData.map((row) => ({
+              nim: row.nim,
+              name: row.name,
+              group: row.group,
+              presence: row.presence as "Hadir" | "Izin/Sakit" | "Alpha",
+            })),
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            message: "Internal server error",
+            data: null,
+          };
+        }
+      },
+    ),
+
+  // Mamet
+  /**
+   * Get the complete presence of all events
+   * @returns TAttendance[]
+   * @WARNING This query is not recommended to be called for the front end as this is a heavy query
+   * and no pagination is implemented
+   */
+  getCompletePresence: publicProcedure.query<
+    TPresenceBaseResponse<TAttendance[]>
+  >(async () => {
+    try {
+      const q = await db
         .select({
           nim: users.nim,
           name: profiles.name,
           group: profiles.group,
           presence: eventPresences.presenceType,
+          day: events.day,
+          openingOrClosing: eventPresences.presenceEvent,
         })
         .from(events)
         .innerJoin(eventPresences, eq(events.id, eventPresences.eventId))
         .innerJoin(users, eq(users.nim, eventPresences.userNim))
-        .innerJoin(profiles, eq(profiles.userId, users.id))
-        .where(
-          and(
-            eq(events.id, eventId),
-            groupName ? eq(profiles.group, groupName) : undefined,
-          ),
-        );
+        .innerJoin(profiles, eq(profiles.userId, users.id));
 
-      return returned;
-    }),
+      const groupingMap = new Map<
+        {
+          day: "Day 1" | "Day 2" | "Day 3" | "Day 4";
+          openingOrClosing: "Opening" | "Closing";
+        },
+        TAttendanceOfAnEvent
+      >();
 
-  // Mamet
-  getCompletePresence: publicProcedure.query<TAttendance[]>(async () => {
-    const q = await db
-      .select({
-        nim: users.nim,
-        name: profiles.name,
-        group: profiles.group,
-        presence: eventPresences.presenceType,
-        day: events.day,
-        openingOrClosing: eventPresences.presenceEvent,
-      })
-      .from(events)
-      .innerJoin(eventPresences, eq(events.id, eventPresences.eventId))
-      .innerJoin(users, eq(users.nim, eventPresences.userNim))
-      .innerJoin(profiles, eq(profiles.userId, users.id));
+      q.forEach((row) => {
+        const key = {
+          day: row.day as "Day 1" | "Day 2" | "Day 3" | "Day 4",
+          openingOrClosing: row.openingOrClosing as "Opening" | "Closing",
+        };
 
-    const groupingMap = new Map<
-      {
-        day: "Day 1" | "Day 2" | "Day 3" | "Day 4";
-        openingOrClosing: "Opening" | "Closing";
-      },
-      TAttendanceOfAnEvent
-    >();
+        if (!groupingMap.has(key)) {
+          groupingMap.set(key, []);
+        }
 
-    q.forEach((row) => {
-      const key = {
-        day: row.day as "Day 1" | "Day 2" | "Day 3" | "Day 4",
-        openingOrClosing: row.openingOrClosing as "Opening" | "Closing",
+        groupingMap.get(key)?.push({
+          nim: row.nim,
+          name: row.name,
+          group: row.group,
+          presence: row.presence as "Hadir" | "Izin/Sakit" | "Alpha",
+        });
+      });
+
+      const returned: TAttendance[] = [];
+      groupingMap.forEach((value, key) => {
+        returned.push({
+          day: key.day,
+          openingOrClosing: key.openingOrClosing,
+          attendance: value,
+        });
+      });
+
+      return {
+        ok: true,
+        message: "Success get all presence",
+        data: returned,
       };
-
-      if (!groupingMap.has(key)) {
-        groupingMap.set(key, []);
-      }
-
-      groupingMap.get(key)?.push({
-        nim: row.nim,
-        name: row.name,
-        group: row.group,
-        presence: row.presence as "Hadir" | "Izin/Sakit" | "Alpha",
-      });
-    });
-
-    const returned: TAttendance[] = [];
-    groupingMap.forEach((value, key) => {
-      returned.push({
-        day: key.day,
-        openingOrClosing: key.openingOrClosing,
-        attendance: value,
-      });
-    });
-
-    return returned;
+    } catch (error) {
+      return {
+        ok: false,
+        message: "Internal server error",
+        data: null,
+      };
+    }
   }),
 
   // Mamet
+  // Status: Tested
   editPresence: publicProcedure
     .input(
       z.object({
         eventId: z.string(),
         nim: z.string(),
-        presence: z.union([
-          z.literal("Hadir"),
-          z.literal("Izin/Sakit"),
-          z.literal("Alpha"),
-        ]),
+        presence: z.enum(["Hadir", "Izin/Sakit", "Alpha"]),
+        openingOrClosing: z.enum(["Opening", "Closing"]),
       }),
     )
-    .mutation(async ({ input: { eventId, nim, presence } }) => {
-      await db
-        .update(eventPresences)
-        .set({
-          presenceType: presence,
-        })
-        .where(
-          and(
-            eq(eventPresences.eventId, eventId),
-            eq(eventPresences.userNim, nim),
-          ),
-        );
+    .mutation(
+      async ({ input: { eventId, nim, presence, openingOrClosing } }) => {
+        try {
+          await db
+            .update(eventPresences)
+            .set({
+              presenceType: presence,
+            })
+            .where(
+              and(
+                eq(eventPresences.eventId, eventId),
+                eq(eventPresences.userNim, nim),
+                eq(eventPresences.presenceEvent, openingOrClosing),
+              ),
+            );
+          return {
+            ok: true,
+            message: "Success edit presence",
+            data: {
+              eventId,
+              nim,
+              presence,
+              openingOrClosing,
+            },
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            message: "Internal server error",
+            data: null,
+          };
+        }
+      },
+    ),
+
+  // Mamet
+  // Status: Tested
+  addPresence: publicProcedure
+    .input(
+      z.object({
+        eventId: z.string(),
+        openingOrClosing: z.enum(["Opening", "Closing"]),
+      }),
+    )
+    .mutation<TPresenceBaseResponse<null>>(
+      async ({ input: { eventId, openingOrClosing } }) => {
+        try {
+          const rolePesertaUsers = await db
+            .select({ nim: users.nim })
+            .from(users)
+            .where(eq(users.role, "Peserta"));
+
+          // Now we have all the nim of the Peserta
+          // We can insert them all into the eventPresences table
+          const insertData = rolePesertaUsers.map((row) => ({
+            eventId,
+            userNim: row.nim,
+            presenceType: "Alpha" as "Hadir" | "Izin/Sakit" | "Alpha",
+            presenceEvent: openingOrClosing,
+            updatedAt: new Date(),
+          }));
+
+          await db.insert(eventPresences).values(insertData);
+
+          return {
+            ok: true,
+            message: "Success add presence",
+            data: null,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            message: "Internal server error",
+            data: null,
+          };
+        }
+      },
+    ),
+
+  // Mamet
+  // Status: Tested
+  /**
+   * Delete the presence of an event
+   * @param eventId The ID of the event
+   * @param openingOrClosing The opening or closing event
+   */
+  deletePresence: publicProcedure
+    .input(
+      z.object({
+        eventId: z.string(),
+        openingOrClosing: z.enum(["Opening", "Closing"]),
+      }),
+    )
+    .mutation<TPresenceBaseResponse<null>>(
+      async ({ input: { eventId, openingOrClosing } }) => {
+        try {
+          await db
+            .delete(eventPresences)
+            .where(
+              and(
+                eq(eventPresences.eventId, eventId),
+                eq(eventPresences.presenceEvent, openingOrClosing),
+              ),
+            );
+
+          return {
+            ok: true,
+            message: "Success delete presence",
+            data: null,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            message: "Internal server error",
+            data: null,
+          };
+        }
+      },
+    ),
+
+  // Mamet
+  // Status: Tested
+  /**
+   * Get the presence of an event
+   * @param eventId The ID of the event
+   * @param openingOrClosing The opening or closing event
+   * @param page The page number (1-indexed)
+   * @param dataPerPage The number of data per page
+   */
+  getPresenceOfAnEvent: publicProcedure
+    .input(
+      z.object({
+        eventId: z.string(),
+        openingOrClosing: z.enum(["Opening", "Closing"]),
+        page: z.number(),
+        dataPerPage: z.number(),
+      }),
+    )
+    .query<TPresenceBaseResponse<TAttendanceOfAnEvent>>(
+      async ({ input: { eventId, openingOrClosing, page, dataPerPage } }) => {
+        try {
+          const returned = await db
+            .select({
+              nim: users.nim,
+              name: profiles.name,
+              group: profiles.group,
+              presence: eventPresences.presenceType,
+            })
+            .from(events)
+            .innerJoin(eventPresences, eq(events.id, eventPresences.eventId))
+            .innerJoin(users, eq(users.nim, eventPresences.userNim))
+            .innerJoin(profiles, eq(profiles.userId, users.id))
+            .where(
+              and(
+                eq(events.id, eventId),
+                eq(eventPresences.presenceEvent, openingOrClosing),
+              ),
+            );
+
+          const paginatedData = returned.slice(
+            dataPerPage * (page - 1),
+            dataPerPage * page,
+          );
+          const data = paginatedData.map((row) => ({
+            nim: row.nim,
+            name: row.name,
+            group: row.group,
+            presence: row.presence as "Hadir" | "Izin/Sakit" | "Alpha",
+          }));
+
+          return {
+            ok: true,
+            message: "Success get presence",
+            data,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            message: "Internal server error",
+            data: null,
+          };
+        }
+      },
+    ),
+
+  // Mamet
+  getPresenceOfAnEventCSV: publicProcedure
+    .input(
+      z.object({
+        eventId: z.string(),
+        openingOrClosing: z.enum(["Opening", "Closing"]),
+      }),
+    )
+    .query<
+      TPresenceBaseResponse<{
+        filename: string;
+        mimeType: string;
+        content: string;
+      }>
+    >(async ({ input: { eventId, openingOrClosing } }) => {
+      try {
+        const returned = await db
+          .select({
+            nim: users.nim,
+            name: profiles.name,
+            group: profiles.group,
+            presence: eventPresences.presenceType,
+          })
+          .from(events)
+          .innerJoin(eventPresences, eq(events.id, eventPresences.eventId))
+          .innerJoin(users, eq(users.nim, eventPresences.userNim))
+          .innerJoin(profiles, eq(profiles.userId, users.id))
+          .where(
+            and(
+              eq(events.id, eventId),
+              eq(eventPresences.presenceEvent, openingOrClosing),
+            ),
+          );
+
+        const data = returned
+          .map((row) => `${row.nim},${row.name},${row.group},${row.presence}`)
+          .join("\n");
+
+        const headers = ["NIM", "Nama", "Kelompok", "Kehadiran"];
+        const csvContent = [headers.join(","), data].join("\n");
+
+        return {
+          ok: true,
+          message: "Success get presence",
+          data: {
+            filename: `presence_${eventId}_${openingOrClosing}.csv`,
+            mimeType: "text/csv",
+            content: csvContent,
+          },
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          message: "Internal server error",
+          data: null,
+        };
+      }
     }),
 });
