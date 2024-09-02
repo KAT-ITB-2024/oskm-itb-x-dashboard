95606be296232c10c92d3f013053d19a76cd3df0
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-09-02 08:12:55 +0700

    chore: removed unused procedures on presence router

--- src/server/api/routers/presence.ts
+++ src/server/api/routers/presence.ts
@@ -11,17 +11,13 @@ import {
   roleEnum,
   users,
 } from "@katitb2024/database";
-import { and, asc, eq, ilike, inArray } from "drizzle-orm";
+import { and, eq, ilike, inArray } from "drizzle-orm";
 import { z } from "zod";
 
 import {
   createTRPCRouter,
-  publicProcedure,
-  //   mametProcedure,
   mentorProcedure,
-  //   mentorMametProcedure,
 } from "~/server/api/trpc";
-import { db } from "~/server/db";
 import { TRPCError } from "@trpc/server";
 
 export type TAttendanceOfAnEvent = {
@@ -246,171 +242,5 @@ export const presenceRouter = createTRPCRouter({
       return {
         message: "Presences successfully inserted/updated",
       };
-    }),
-
-  // Mamet
-  // Status: Tested
-  /**
-   * Delete the presence of an event
-   * @param eventId The ID of the event
-   * @param openingOrClosing The opening or closing event
-   */
-  deletePresence: publicProcedure
-    .input(
-      z.object({
-        eventId: z.string(),
-        openingOrClosing: z.enum(["Opening", "Closing"]),
-      }),
-    )
-    .mutation<TPresenceBaseResponse<null>>(
-      async ({ input: { eventId, openingOrClosing } }) => {
-        try {
-          await db
-            .delete(eventPresences)
-            .where(
-              and(
-                eq(eventPresences.eventId, eventId),
-                eq(eventPresences.presenceEvent, openingOrClosing),
-              ),
-            );
-
-          return {
-            ok: true,
-            message: "Success delete presence",
-            data: null,
-          };
-        } catch (error) {
-          return {
-            ok: false,
-            message: "Internal server error",
-            data: null,
-          };
-        }
-      },
-    ),
-
-  // Mamet
-  // Status: Tested
-  /**
-   * Get the presence of an event
-   * @param eventId The ID of the event
-   * @param openingOrClosing The opening or closing event
-   * @param page The page number (1-indexed)
-   * @param dataPerPage The number of data per page
-   */
-  getPresenceOfAnEvent: publicProcedure
-    .input(
-      z.object({
-        eventId: z.string(),
-        openingOrClosing: z.enum(["Opening", "Closing"]),
-        page: z.number(),
-        dataPerPage: z.number(),
-      }),
-    )
-    .query<TPresenceBaseResponse<TAttendanceOfAnEvent>>(
-      async ({ input: { eventId, openingOrClosing, page, dataPerPage } }) => {
-        try {
-          const returned = await db
-            .select({
-              nim: users.nim,
-              name: profiles.name,
-              group: profiles.group,
-              presence: eventPresences.presenceType,
-            })
-            .from(events)
-            .innerJoin(eventPresences, eq(events.id, eventPresences.eventId))
-            .innerJoin(users, eq(users.nim, eventPresences.userNim))
-            .innerJoin(profiles, eq(profiles.userId, users.id))
-            .where(
-              and(
-                eq(events.id, eventId),
-                eq(eventPresences.presenceEvent, openingOrClosing),
-              ),
-            );
-
-          const paginatedData = returned.slice(
-            dataPerPage * (page - 1),
-            dataPerPage * page,
-          );
-          const data = paginatedData.map((row) => ({
-            nim: row.nim,
-            name: row.name,
-            group: row.group,
-            presence: row.presence,
-          }));
-
-          return {
-            ok: true,
-            message: "Success get presence",
-            data,
-          };
-        } catch (error) {
-          return {
-            ok: false,
-            message: "Internal server error",
-            data: null,
-          };
-        }
-      },
-    ),
-
-  // Mamet
-  // Status: Tested
-  getPresenceOfAnEventCSV: publicProcedure
-    .input(
-      z.object({
-        eventId: z.string(),
-        openingOrClosing: z.enum(["Opening", "Closing"]),
-      }),
-    )
-    .query<
-      TPresenceBaseResponse<{
-        filename: string;
-        mimeType: string;
-        content: string;
-      }>
-    >(async ({ input: { eventId, openingOrClosing } }) => {
-      try {
-        const returned = await db
-          .select({
-            nim: users.nim,
-            name: profiles.name,
-            group: profiles.group,
-            presence: eventPresences.presenceType,
-          })
-          .from(events)
-          .innerJoin(eventPresences, eq(events.id, eventPresences.eventId))
-          .innerJoin(users, eq(users.nim, eventPresences.userNim))
-          .innerJoin(profiles, eq(profiles.userId, users.id))
-          .where(
-            and(
-              eq(events.id, eventId),
-              eq(eventPresences.presenceEvent, openingOrClosing),
-            ),
-          );
-
-        const data = returned
-          .map((row) => `${row.nim},${row.name},${row.group},${row.presence}`)
-          .join("\n");
-
-        const headers = ["NIM", "Nama", "Kelompok", "Kehadiran"];
-        const csvContent = [headers.join(","), data].join("\n");
-
-        return {
-          ok: true,
-          message: "Success get presence",
-          data: {
-            filename: `presence_${eventId}_${openingOrClosing}_.csv`,
-            mimeType: "text/csv",
-            content: csvContent,
-          },
-        };
-      } catch (error) {
-        return {
-          ok: false,
-          message: "Internal server error",
-          data: null,
-        };
-      }
     }),
 });
