621693c48032ba40f2734541e29c524522da4ecc
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-09-02 08:10:21 +0700

    chore: added upsertGroupPresenceData

--- src/server/api/routers/presence.ts
+++ src/server/api/routers/presence.ts
@@ -46,183 +46,207 @@ export type TPresenceBaseResponse<T> = {
 export const presenceRouter = createTRPCRouter({
   // Mentor
   getPresenceOfAGroupInAnEvent: mentorProcedure
-  .input(
-    z.object({
-      eventId: z.string(),
-      groupName: z.string(),
-      presenceEvent: z.enum([...presenceEventEnum.enumValues]),
-      page: z.number().min(1).default(1), // Pagination: page number
-      limit: z.number().min(1).max(100).default(10), // Pagination: limit per page
-      search: z.string().optional(), // Search query for user's name
-    })
-  )
-  .query(async ({ ctx, input }) => {
-    const { groupName, eventId, presenceEvent, page, limit, search } = input;
-
-    const groupExists = await ctx.db
-      .select()
-      .from(profiles)
-      .where(eq(profiles.group, groupName))
-      .limit(1);
-
-    if (!groupExists.length) {
-      throw new TRPCError({
-        code: "NOT_FOUND",
-        message: "Kelompok/group not found",
-      });
-    }
-
-    const eventExists = await ctx.db
-      .select()
-      .from(events)
-      .where(eq(events.id, eventId))
-      .limit(1);
-
-    if (!eventExists.length) {
-      throw new TRPCError({
-        code: "NOT_FOUND",
-        message: "Event not found",
-      });
-    }
-
-    // Fetch users in the specified group with the role "Peserta"
-    const usersQuery = ctx.db
-      .select({
-        nim: users.nim,
-        name: profiles.name,
-        userId: users.id,
-      })
-      .from(users)
-      .innerJoin(profiles, eq(users.id, profiles.userId))
-      .where(and(
-        eq(profiles.group, groupName),
-        eq(users.role, roleEnum.enumValues[0]),
-        ilike(profiles.name, `%${search ? search : ""}%`)
-      ));
-
-    const usersInGroup = await usersQuery;
-
-    if (!usersInGroup.length) {
-      return [];
-    }
-
-    const nimValues = usersInGroup.map((user) => user.nim);
-
-    // Fetch matching presences for the event
-    const matchingPresences = await ctx.db
-      .select({
-        id: eventPresences.id,
-        createdAt: eventPresences.createdAt,
-        updatedAt: eventPresences.updatedAt,
-        presenceType: eventPresences.presenceType,
-        presenceEvent: eventPresences.presenceEvent,
-        userNim: eventPresences.userNim,
-        eventId: eventPresences.eventId,
-      })
-      .from(eventPresences)
-      .where(
-        and(
-          eq(eventPresences.eventId, eventId),
-          eq(eventPresences.presenceEvent, presenceEvent),
-          inArray(eventPresences.userNim, nimValues) // use the inArray method instead of raw SQL
-        )
-      );
-
-    // Mapping presence data with user names
-    const matchingPresencesWithName = matchingPresences.map((presence) => {
-      const user = usersInGroup.find((user) => user.nim === presence.userNim);
-      return {
-        ...presence,
-        name: user?.name,
-      };
-    });
-
-    // Creating Alpha presences for users not found in eventPresences
-    const alphaPresences = usersInGroup.map((user) => ({
-      id: crypto.randomUUID(), // Generating a random unique ID
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      presenceType: presenceTypeEnum.enumValues[2], // Alpha
-      presenceEvent: presenceEvent,
-      userNim: user.nim,
-      name: user.name, // Using the user's name from profile
-      eventId: eventId,
-    }));
-
-    // Merging the presence data with Alpha data if no presence is found
-    const presences = usersInGroup.map((user) => {
-      const presence = matchingPresencesWithName.find(
-        (p) => p.userNim === user.nim
-      );
-
-      if (presence) {
-        return presence;
-      } else {
-        return alphaPresences.find((p) => p.userNim === user.nim)!;
-      }
-    });
-
-    // Implement pagination
-    const offset = (page - 1) * limit;
-    const paginatedPresences = presences.slice(offset, offset + limit);
-
-    return {
-      ok: true,
-      data: paginatedPresences,
-      message: "Successfully fetched group presence data.",
-      meta: {
-        page,
-        limit,
-        total: presences.length,
-      },
-    };
-  }),
-
-  // Mentor
-  // Status: Tested
-  editPresence: mentorProcedure
     .input(
       z.object({
         eventId: z.string(),
-        nim: z.string(),
-        presence: z.enum(["Hadir", "Izin/Sakit", "Alpha"]),
-        openingOrClosing: z.enum(["Opening", "Closing"]),
+        groupName: z.string(),
+        presenceEvent: z.enum([...presenceEventEnum.enumValues]),
+        page: z.number().min(1).default(1),
+        limit: z.number().min(1).max(100).default(10),
+        search: z.string().optional(),
       }),
     )
-    .mutation(
-      async ({ input: { eventId, nim, presence, openingOrClosing } }) => {
-        try {
-          await db
-            .update(eventPresences)
-            .set({
-              presenceType: presence,
-            })
-            .where(
-              and(
-                eq(eventPresences.eventId, eventId),
-                eq(eventPresences.userNim, nim),
-                eq(eventPresences.presenceEvent, openingOrClosing),
-              ),
-            );
-          return {
-            ok: true,
-            message: "Success edit presence",
-            data: {
-              eventId,
-              nim,
-              presence,
-              openingOrClosing,
-            },
-          };
-        } catch (error) {
-          return {
-            ok: false,
-            message: "Internal server error",
-            data: null,
-          };
+    .query(async ({ ctx, input }) => {
+      const { groupName, eventId, presenceEvent, page, limit, search } = input;
+
+      const groupExists = await ctx.db
+        .select()
+        .from(profiles)
+        .where(eq(profiles.group, groupName))
+        .limit(1);
+
+      if (!groupExists.length) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Kelompok/group not found",
+        });
+      }
+
+      const eventExists = await ctx.db
+        .select()
+        .from(events)
+        .where(eq(events.id, eventId))
+        .limit(1);
+
+      if (!eventExists.length) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Event not found",
+        });
+      }
+
+      // Fetch users in the specified group with the role "Peserta"
+      const usersQuery = ctx.db
+        .select({
+          nim: users.nim,
+          name: profiles.name,
+          userId: users.id,
+        })
+        .from(users)
+        .innerJoin(profiles, eq(users.id, profiles.userId))
+        .where(
+          and(
+            eq(profiles.group, groupName),
+            eq(users.role, roleEnum.enumValues[0]),
+            ilike(profiles.name, `%${search ? search : ""}%`),
+          ),
+        );
+
+      const usersInGroup = await usersQuery;
+
+      if (!usersInGroup.length) {
+        return [];
+      }
+
+      const nimValues = usersInGroup.map((user) => user.nim);
+
+      // Fetch matching presences for the event
+      const matchingPresences = await ctx.db
+        .select({
+          id: eventPresences.id,
+          createdAt: eventPresences.createdAt,
+          updatedAt: eventPresences.updatedAt,
+          presenceType: eventPresences.presenceType,
+          presenceEvent: eventPresences.presenceEvent,
+          userNim: eventPresences.userNim,
+          eventId: eventPresences.eventId,
+        })
+        .from(eventPresences)
+        .where(
+          and(
+            eq(eventPresences.eventId, eventId),
+            eq(eventPresences.presenceEvent, presenceEvent),
+            inArray(eventPresences.userNim, nimValues),
+          ),
+        );
+
+      // Mapping presence data with user names
+      const matchingPresencesWithName = matchingPresences.map((presence) => {
+        const user = usersInGroup.find((user) => user.nim === presence.userNim);
+        return {
+          ...presence,
+          name: user?.name,
+        };
+      });
+
+      // Creating Alpha presences for users not found in eventPresences
+      const alphaPresences = usersInGroup.map((user) => ({
+        id: crypto.randomUUID(), // Generating a random unique ID
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        presenceType: presenceTypeEnum.enumValues[2],
+        presenceEvent: presenceEvent,
+        userNim: user.nim,
+        name: user.name,
+        eventId: eventId,
+      }));
+
+      // Merging the presence data with Alpha data if no presence is found
+      const presences = usersInGroup.map((user) => {
+        const presence = matchingPresencesWithName.find(
+          (p) => p.userNim === user.nim,
+        );
+
+        if (presence) {
+          return presence;
+        } else {
+          return alphaPresences.find((p) => p.userNim === user.nim)!;
         }
-      },
-    ),
+      });
+
+      // Implement pagination
+      const offset = (page - 1) * limit;
+      const paginatedPresences = presences.slice(offset, offset + limit);
+
+      return {
+        ok: true,
+        data: paginatedPresences,
+        message: "Successfully fetched group presence data.",
+        meta: {
+          page,
+          limit,
+          total: presences.length,
+        },
+      };
+    }),
+
+  // Mentor
+  // Status: Tested
+  upsertGroupPresenceData: mentorProcedure
+    .input(
+      z.object({
+        eventId: z.string(),
+        presenceEvent: z.enum([...presenceEventEnum.enumValues]),
+        presences: z.array(
+          z.object({
+            userNim: z.string(),
+            presenceType: z.enum([...presenceTypeEnum.enumValues]),
+            keterangan: z.string().optional(),
+          }),
+        ),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { eventId, presenceEvent, presences } = input;
+
+      // Check if the event exists
+      const eventExists = await ctx.db
+        .select()
+        .from(events)
+        .where(eq(events.id, eventId));
+
+      if (eventExists.length === 0) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Event not found",
+        });
+      }
+
+      // Prepare the values to be inserted or updated
+      const valuesToInsert = presences.map((presence) => ({
+        eventId,
+        userNim: presence.userNim,
+        presenceType: presence.presenceType,
+        presenceEvent,
+        remarks: presence.keterangan ?? null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }));
+
+      await ctx.db.transaction(async (tx) => {
+        for (const value of valuesToInsert) {
+          await tx
+            .insert(eventPresences)
+            .values(value)
+            .onConflictDoUpdate({
+              target: [
+                eventPresences.eventId,
+                eventPresences.presenceEvent,
+                eventPresences.userNim,
+              ],
+              set: {
+                presenceType: value.presenceType,
+                remarks: value.remarks,
+                updatedAt: new Date(),
+              },
+            });
+        }
+      });
+
+      return {
+        message: "Presences successfully inserted/updated",
+      };
+    }),
 
   // Mamet
   // Status: Tested
@@ -388,126 +412,5 @@ export const presenceRouter = createTRPCRouter({
           data: null,
         };
       }
-    }),
-
-  // mendapat list presensi peserta pada sesuai keluarga dan event
-  getPresensiPeserta: publicProcedure
-    .input(
-      z.object({
-        eventId: z.string(),
-        group: z.string(),
-      }),
-    )
-    .query(async ({ ctx, input }) => {
-      const presensiPeserta = await ctx.db
-        .select({
-          nim: users.nim,
-          nama: profiles.name,
-          status: eventPresences.presenceType,
-          updatedAt: eventPresences.updatedAt,
-        })
-        .from(eventPresences)
-        .leftJoin(events, eq(events.id, eventPresences.eventId))
-        .leftJoin(users, eq(users.nim, eventPresences.userNim))
-        .leftJoin(profiles, eq(profiles.userId, users.id))
-        .where(
-          and(
-            eq(profiles.group, input.group),
-            eq(users.role, roleEnum.enumValues[0]),
-            eq(eventPresences.eventId, input.eventId),
-          ),
-        );
-
-      return presensiPeserta;
-    }),
-
-  updatePresensiPeserta: publicProcedure
-    .input(
-      z.object({
-        eventId: z.string(),
-        userNim: z.string(),
-        newPresenceType: z.enum(["Hadir", "Izin/Sakit", "Alpha"]),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      // Expected output: update status presensi peserta pada suatu kelompok
-
-      // check if event exist
-      const event = await ctx.db
-        .select()
-        .from(events)
-        .where(eq(events.id, input.eventId));
-
-      if (event.length == 0) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Event not found",
-        });
-      }
-
-      // check if event exist
-      const user = await ctx.db
-        .select()
-        .from(users)
-        .where(eq(users.nim, input.userNim));
-
-      if (user.length == 0) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "User not found",
-        });
-      }
-
-      await ctx.db
-        .update(eventPresences)
-        .set({
-          presenceType: input.newPresenceType,
-          updatedAt: new Date(),
-        })
-        .where(
-          and(
-            eq(eventPresences.userNim, input.userNim),
-            eq(eventPresences.eventId, input.eventId),
-          ),
-        );
-
-      return { message: "Presence successfully updated" };
-    }),
-  getEventsThatHasPresence: publicProcedure
-    .input(
-      z.object({
-        page: z.number(),
-        dataPerPage: z.number(),
-      }),
-    )
-    .query(async ({ input: { page, dataPerPage } }) => {
-      const eventsWithPresence = await db
-        .selectDistinct({
-          eventId: events.id,
-          eventDay: events.day,
-          eventDate: events.eventDate,
-          openingOpenPresenceTime: events.openingOpenPresenceTime,
-          openingClosePresenceTime: events.openingClosePresenceTime,
-          closingOpenPresenceTime: events.closingOpenPresenceTime,
-          closingClosePresenceTime: events.closingClosePresenceTime,
-        })
-        .from(events)
-        .leftJoin(eventPresences, eq(events.id, eventPresences.eventId))
-        .orderBy(asc(events.eventDate));
-
-      // Calculate total number of items (before slicing)
-      const totalItems = eventsWithPresence.length;
-
-      // Apply pagination
-      const paginatedData = eventsWithPresence.slice(
-        dataPerPage * (page - 1),
-        dataPerPage * page,
-      );
-
-      // Return both paginated data and total item count
-      return {
-        paginatedData,
-        totalItems,
-      };
     }),
 });
